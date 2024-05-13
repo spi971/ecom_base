fc35cd49fed39a80f30e2daeee86b4db52c2e49f
spi971 <sandypierre@protonmail.com>
2024-05-13 15:22:39 +0200

    ADD admin navigation && clean useless default images and favicon

---
+++ app/admin/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'></div>;
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,9 @@
-import { notFound } from "next/navigation";
-
-const getData = async () => {
-  const res = await fetch(`${process.env.API_URL}posts`, {
-    cache: "no-store",
-  });
-  if (!res.ok) return notFound();
-  return res.json();
-};
 
 export default async function Home() {
-  const data = await getData();
   
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Data List</h1>
-      <ul>
-        {data.map((item: any) => (
-          <li key={item.id} className='mb-5'>
-            <h2>{item.title}</h2>
-            <p>User ID: {item.userId}</p>
-            <p>ID: {item.id}</p>
-            <p>Description: {item.body}</p>
-          </li>
-        ))}
-      </ul>
+      <h1>Hi Ecommerce devs!</h1>
     </main>
   );
 }
